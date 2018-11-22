ecff41efb1130733b57117f495a9eaefab737cd5
taochen <taochen@baina.com>
2018-11-22 18:13:13 +0800

    [feature] 组件化第一步[开发测试模式(library)发布模式(module)切换]

---
+++ app1/src/main/java/baina/android/com/app1/PluginTest1Activity.java
@@ -0,0 +1,13 @@
+package baina.android.com.app1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PluginTest1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plugin_test1);
+    }
+}
